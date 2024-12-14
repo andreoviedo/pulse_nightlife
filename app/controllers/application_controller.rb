ff081c9d5810ea4741472a4999c75ef543040d46
Andre Oviedo <aroviedom@gmail.com>
2024-12-14 19:00:34 +0000

    gooooo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number, :date_of_birth])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone_number, :date_of_birth])
+  end
 end
---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["menu"]
+
+  toggle() {
+    this.menuTarget.classList.toggle("hidden")
+  }
+
+  // Hide dropdown when clicking outside
+  connect() {
+    document.addEventListener("click", this.handleClickOutside.bind(this))
+  }
+
+  disconnect() {
+    document.removeEventListener("click", this.handleClickOutside.bind(this))
+  }
+
+  handleClickOutside(event) {
+    if (!this.element.contains(event.target)) {
+      this.menuTarget.classList.add("hidden")
+    }
+  }
+} 
